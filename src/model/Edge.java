b9ae3011726a7bbab89c766d0d3e74b208797f8c
mxhke <84710206+mxhke@users.noreply.github.com>
2021-05-29 13:19:06 +0200

    Add files via upload

---
+++ src/model/Edge.java
@@ -0,0 +1,44 @@
+package model;
+
+
+public class Edge 
+{
+    private Vertex ersterPunkt;
+    private Vertex zweiterPunkt;
+    
+    private double laenge;
+    
+    public Edge(Vertex ersterPunkt, Vertex zweiterPunkt)
+    {
+        this.ersterPunkt = ersterPunkt;
+        this.zweiterPunkt = zweiterPunkt;
+        this.laenge = this.berechneLaenge();
+    }
+    
+    
+    
+    
+    
+    private double berechneLaenge()
+    {
+        return Math.sqrt(Math.pow((this.zweiterPunkt.getXKoordinate()
+                                    -this.ersterPunkt.getXKoordinate()), 2)
+                        +Math.pow((this.zweiterPunkt.getYKoordinate()
+                                    -this.ersterPunkt.getYKoordinate()), 2)
+                        +Math.pow((this.zweiterPunkt.getZKoordinate()
+                                    -this.ersterPunkt.getZKoordinate()), 2));
+    }
+    
+    
+    
+    
+    
+    
+    // Getter 
+    public double getLaenge()
+    {
+        return this.laenge;
+    }
+    
+    
+}
---
+++ src/model/Polyeder.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.Vector;
+
+
+public class Polyeder 
+{
+    // Bezeichnung des Polyeders
+    private String bezeichnung;
+    
+    // Die einzelnen Dreiecke des Polyeders
+    private Vector<Trigon> trigone;
+    
+    // Volumen des Polyeders
+    private double volumen;
+    
+    // Oberflächeinhalt des Polyeders
+    private double oberflaecheninhalt;
+    
+    private int anzahlflaechen; 
+    
+     
+}
